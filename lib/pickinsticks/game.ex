e70e977c56745c60197c2c43063d99ab20197ae9
Guilhem Bernard-Floyd <g.bernardfloyd@gmail.com>
2020-12-06 10:53:34 +0100

    move state related functions to state module

--- lib/pickinsticks/game.ex
+++ lib/pickinsticks/game.ex
@@ -21,22 +21,11 @@ defmodule Pickinsticks.Game do
 
   def make_move(state, direction) do
     state
-    |> move(direction)
+    |> State.move(direction)
     |> check_colisions
-    |> update_state(state)
-    |> check_win
+    |> State.update_state(state)
+    |> State.check_win()
   end
-
-  defp move(%State{player_position: pp} = state, :up), do: move_player_at(state, pp.x, pp.y - 1)
-  defp move(%State{player_position: pp} = state, :down), do: move_player_at(state, pp.x, pp.y + 1)
-  defp move(%State{player_position: pp} = state, :left), do: move_player_at(state, pp.x - 1, pp.y)
-
-  defp move(%State{player_position: pp} = state, :right),
-    do: move_player_at(state, pp.x + 1, pp.y)
-
-  defp move(state, _), do: state
-
-  defp move_player_at(state, x, y), do: %State{state | player_position: Pos.at(x, y)}
 
   defp check_colisions(state) do
     cond do
@@ -62,20 +51,4 @@ defmodule Pickinsticks.Game do
       true -> false
     end
   end
-
-  defp update_state({:stick, state}, _current_state), do: pick_stick(state)
-  defp update_state({:blocked, _}, current_state), do: current_state
-  defp update_state({:out_of_bounds, _}, current_state), do: current_state
-  defp update_state({:ok, state}, _), do: state
-
-  defp pick_stick(%State{player_position: ppos, sticks_found: count} = state) do
-    %State{
-      state
-      | sticks: Enum.reject(state.sticks, fn x -> x == ppos end),
-        sticks_found: count + 1
-    }
-  end
-
-  defp check_win(%State{sticks: []} = state), do: %State{state | won: true}
-  defp check_win(state), do: state
 end
--- lib/pickinsticks/state.ex
+++ lib/pickinsticks/state.ex
@@ -1,4 +1,5 @@
 defmodule Pickinsticks.State do
+  alias Pickinsticks.State
   alias Pickinsticks.Position, as: Pos
 
   defstruct(
@@ -8,4 +9,31 @@ defmodule Pickinsticks.State do
     sticks_found: 0,
     won: false
   )
+
+  def update_state({:stick, state}, _current_state), do: pick_stick(state)
+  def update_state({:blocked, _}, current_state), do: current_state
+  def update_state({:out_of_bounds, _}, current_state), do: current_state
+  def update_state({:ok, state}, _), do: state
+
+  defp pick_stick(%State{player_position: pp, sticks_found: count} = state) do
+    %State{
+      state
+      | sticks: Enum.reject(state.sticks, fn x -> x == pp end),
+        sticks_found: count + 1
+    }
+  end
+
+  def move(%State{player_position: pp} = state, :up), do: move_player_at(state, pp.x, pp.y - 1)
+  def move(%State{player_position: pp} = state, :down), do: move_player_at(state, pp.x, pp.y + 1)
+  def move(%State{player_position: pp} = state, :left), do: move_player_at(state, pp.x - 1, pp.y)
+
+  def move(%State{player_position: pp} = state, :right),
+    do: move_player_at(state, pp.x + 1, pp.y)
+
+  def move(state, _), do: state
+
+  defp move_player_at(state, x, y), do: %State{state | player_position: Pos.at(x, y)}
+
+  def check_win(%State{sticks: []} = state), do: %State{state | won: true}
+  def check_win(state), do: state
 end
