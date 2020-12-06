aa4a9cc4faafb726649bc8ddb36b79c134a3d959
Guilhem Bernard-Floyd <g.bernardfloyd@gmail.com>
2020-12-06 10:57:24 +0100

    colision module

---
+++ lib/pickinsticks/colision.ex
@@ -0,0 +1,28 @@
+defmodule Pickinsticks.Colision do
+  alias Pickinsticks.Position, as: Pos
+
+  def check_colisions(state) do
+    cond do
+      player_on_stick?(state) -> {:stick, state}
+      player_blocked?(state) -> {:blocked, state}
+      player_out_of_bounds?(state) -> {:out_of_bounds, state}
+      true -> {:ok, state}
+    end
+  end
+
+  defp player_on_stick?(state) do
+    Enum.any?(state.sticks, &Pos.same?(&1, state.player_position))
+  end
+
+  defp player_blocked?(_state), do: false
+
+  defp player_out_of_bounds?(state) do
+    cond do
+      state.player_position.x == -1 -> true
+      state.player_position.y == -1 -> true
+      state.player_position.x > state.world_bounds.width -> true
+      state.player_position.y > state.world_bounds.height -> true
+      true -> false
+    end
+  end
+end
--- lib/pickinsticks/game.ex
+++ lib/pickinsticks/game.ex
@@ -1,5 +1,5 @@
 defmodule Pickinsticks.Game do
-  alias Pickinsticks.State
+  alias Pickinsticks.{State, Colision}
   alias Pickinsticks.Position, as: Pos
 
   def new_game, do: new_game(1)
@@ -22,33 +22,8 @@ defmodule Pickinsticks.Game do
   def make_move(state, direction) do
     state
     |> State.move(direction)
-    |> check_colisions
+    |> Colision.check_colisions()
     |> State.update_state(state)
     |> State.check_win()
-  end
-
-  defp check_colisions(state) do
-    cond do
-      player_on_stick?(state) -> {:stick, state}
-      player_blocked?(state) -> {:blocked, state}
-      player_out_of_bounds?(state) -> {:out_of_bounds, state}
-      true -> {:ok, state}
-    end
-  end
-
-  defp player_on_stick?(state) do
-    Enum.any?(state.sticks, &Pos.same?(&1, state.player_position))
-  end
-
-  defp player_blocked?(_state), do: false
-
-  defp player_out_of_bounds?(state) do
-    cond do
-      state.player_position.x == -1 -> true
-      state.player_position.y == -1 -> true
-      state.player_position.x > state.world_bounds.width -> true
-      state.player_position.y > state.world_bounds.height -> true
-      true -> false
-    end
   end
 end
