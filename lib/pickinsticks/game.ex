33a64546c70e880c3e672af7dd494413ecc23394
Guilhem Bernard-Floyd <g.bernardfloyd@gmail.com>
2020-12-06 07:13:26 +0100

    remove input handling, use %{x: , y: } struct for positions

--- lib/pickinsticks/game.ex
+++ lib/pickinsticks/game.ex
@@ -2,9 +2,9 @@ defmodule Pickinsticks.Game do
   alias Pickinsticks.Game
 
   defstruct(
-    sticks: [{2, 2}, {3, 0}, {5, 7}, {8, 2}],
-    player_position: {0, 0},
-    world_bounds: {18, 9},
+    sticks: [%{x: 2, y: 2}, %{x: 3, y: 0}, %{x: 5, y: 7}, %{x: 8, y: 2}],
+    player_position: %{x: 0, y: 0},
+    world_bounds: %{width: 18, height: 9},
     sticks_found: 0,
     won: false
   )
@@ -17,7 +17,7 @@ defmodule Pickinsticks.Game do
   end
 
   def random_stick(_) do
-    {Enum.random(0..18), Enum.random(0..9)}
+    %{x: Enum.random(0..18), y: Enum.random(0..9)}
   end
 
   def build_game, do: %Game{}
@@ -26,53 +26,57 @@ defmodule Pickinsticks.Game do
     %Game{sticks: sticks}
   end
 
-  def handle_input(input, state) do
-    input
-    |> make_move(state)
+  def make_move(state, direction) do
+    state
+    |> move(direction)
     |> check_colisions
+    |> update_state(state)
     |> check_win
   end
 
-  defp make_move("k", %Game{player_position: {_, y}} = state) when y > 0 do
-    move_up(state)
-  end
+  defp move(%Game{player_position: pp} = state, :up),
+    do: %Game{state | player_position: %{x: pp.x, y: pp.y - 1}}
 
-  defp make_move("j", %Game{world_bounds: {_, h}, player_position: {_, y}} = state) when y < h do
-    move_down(state)
-  end
+  defp move(%Game{player_position: pp} = state, :down),
+    do: %Game{state | player_position: %{x: pp.x, y: pp.y + 1}}
 
-  defp make_move("h", %Game{player_position: {x, _}} = state) when x > 0 do
-    move_left(state)
-  end
+  defp move(%Game{player_position: pp} = state, :left),
+    do: %Game{state | player_position: %{x: pp.x - 1, y: pp.y}}
 
-  defp make_move("l", %Game{world_bounds: {w, _}, player_position: {x, _}} = state) when x < w do
-    move_right(state)
-  end
+  defp move(%Game{player_position: pp} = state, :right),
+    do: %Game{state | player_position: %{x: pp.x + 1, y: pp.y}}
 
-  defp make_move(_, state), do: state
-
-  defp move_up(%Game{player_position: {x, y}} = state),
-    do: %Game{state | player_position: {x, y - 1}}
-
-  defp move_down(%Game{player_position: {x, y}} = state),
-    do: %Game{state | player_position: {x, y + 1}}
-
-  defp move_left(%Game{player_position: {x, y}} = state),
-    do: %Game{state | player_position: {x - 1, y}}
-
-  defp move_right(%Game{player_position: {x, y}} = state),
-    do: %Game{state | player_position: {x + 1, y}}
+  defp move(state, _), do: state
 
   defp check_colisions(state) do
     cond do
-      player_on_stick?(state) -> pick_stick(state)
-      true -> state
+      player_on_stick?(state) -> {:stick, state}
+      player_blocked?(state) -> {:blocked, state}
+      player_out_of_bounds?(state) -> {:out_of_bounds, state}
+      true -> {:ok, state}
     end
   end
 
   defp player_on_stick?(state) do
     Enum.any?(state.sticks, fn x -> x == state.player_position end)
   end
+
+  defp player_blocked?(_state), do: false
+
+  defp player_out_of_bounds?(state) do
+    cond do
+      state.player_position.x == -1 -> true
+      state.player_position.y == -1 -> true
+      state.player_position.x > state.world_bounds.width -> true
+      state.player_position.y > state.world_bounds.height -> true
+      true -> false
+    end
+  end
+
+  defp update_state({:stick, state}, _current_state), do: pick_stick(state)
+  defp update_state({:blocked, _}, current_state), do: current_state
+  defp update_state({:out_of_bounds, _}, current_state), do: current_state
+  defp update_state({:ok, state}, _), do: state
 
   defp pick_stick(%Game{player_position: ppos, sticks_found: count} = state) do
     %Game{
