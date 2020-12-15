2b4f5f9d8592ff5c85b22b4a50ace65fcdc11b04
Guilhem Bernard-Floyd <g.bernardfloyd@gmail.com>
2020-12-15 07:41:28 +0100

    update game_tests

--- test/game_test.exs
+++ test/game_test.exs
@@ -1,28 +1,26 @@
 defmodule GameTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   alias Pickinsticks.Game
+  alias Pickinsticks.Position, as: Pos
 
   test "new_game returns a new game state" do
-    game = Game.new_game()
+    game = Game.new_game(1)
 
     assert game.won == false
     assert game.sticks_found == 0
-    assert game.player_position == {0,0}
+    assert game.player_position == Pos.at(1, 1)
   end
 
   test "invalid input should not change the state" do
-    game = Game.new_game()
+    game = Game.new_game(1)
 
-    assert ^game = Game.handle_input("invalid", game)
+    assert {_, ^game} = Game.make_move(game, :lol)
   end
 
   test "can't go up when y is 0" do
-    game = Game.new_game()
+    game = Game.new_game(1)
 
-    assert ^game = Game.handle_input("k", game)
+    assert {_, ^game} = Game.make_move(game, :up)
   end
-
-
 end
-
